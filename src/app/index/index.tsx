26274d29e04fe4c0f3aab6ff96fe2c0c954e6fe2
Iagoti <147290042+Iagoti@users.noreply.github.com>
2024-12-19 14:15:09 -0300

    Create estructure

---
+++ src/app/index/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View, StyleSheet } from "react-native";
+import { styles } from "./style";
+export default function Index(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello, World!</Text>
+        </View>
+    ) 
+}
