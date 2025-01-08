608ebda0106eb24d3443d5d3ae38455e86bd9055
Iagoti <147290042+Iagoti@users.noreply.github.com>
2025-01-07 23:05:07 -0300

    Add button and icon

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,16 @@
-import { Text, View, StyleSheet } from "react-native";
-import { styles } from "./style";
+import { View, Image, TouchableOpacity} from "react-native";
+import { styles } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
 export default function Index(){
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Hello, World!</Text>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
         </View>
     ) 
 }
