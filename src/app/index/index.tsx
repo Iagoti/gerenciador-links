2f17c7701fb0becf8eedec85432815d6bddfee60
Iagoti <147290042+Iagoti@users.noreply.github.com>
2025-01-27 22:56:32 -0300

    Create modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,10 @@
-import { View, Image, TouchableOpacity} from "react-native";
+import { View, Image, TouchableOpacity, FlatList, Modal, Text} from "react-native";
 import { styles } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
+import { Option } from "@/components/option";
 export default function Index(){
     return (
         <View style={styles.container}>
@@ -15,7 +16,43 @@ export default function Index(){
             </View>
 
            <Categories />
-           <Link name="Teste Links" url="www.google.com" onDetails={() => console.log("Clicou Link ")} />
+
+           <FlatList
+                data={["1", "2", "3", "4", "5"]}
+                keyExtractor={(item) => item}
+                renderItem={()=> (
+                    <Link 
+                        name="Teste Links" 
+                        url="www.google.com" 
+                        onDetails={() => console.log("Clicou Link ")} 
+                    />
+                )}
+                style={styles.links}
+                contentContainerStyle={styles.linksContent}
+                showsVerticalScrollIndicator={false}
+            />
+
+            <Modal transparent visible={true}>  
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}> Curso </Text>
+                            <TouchableOpacity>
+                            <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+                            </TouchableOpacity>
+                        </View>
+
+                        <Text style={styles.modalLinkName}> RockertSeat </Text>
+                        <Text style={styles.modalUrl}> www.google.com.br </Text>
+
+                        <View style={styles.modalFooter}>
+                            <Option name="Excluir" icon="delete" variant="secondary" />
+                            <Option name="Abrir" icon="language" />
+                        </View>
+                    </View>
+                </View>
+
+            </Modal>
         </View>
     ) 
 }
