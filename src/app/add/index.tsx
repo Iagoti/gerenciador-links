2a5531304cac70ae6aba0b26daa7794136cb8615
Iagoti <147290042+Iagoti@users.noreply.github.com>
2025-02-03 21:40:19 -0300

    Validação de campos

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
@@ -6,8 +6,29 @@ import { router } from "expo-router";
 import { Categories } from "@/components/categories";
 import { Input } from "@/components/input";
 import { Button } from "@/components/button";
+import { useState } from "react";
 
 export default function add(){
+
+    const [category, setCategory] = useState("");
+    const [name, setName] = useState("");
+    const [url, setUrl] = useState("");
+
+    function handleAdd(){
+        if(!category){
+            return Alert.alert("Categoria","Selecione uma categoria");
+        }
+
+        if(!name.trim()){
+            return Alert.alert("Nome", "Informe o nome do item");
+        }
+
+        if(!url.trim()){
+            return Alert.alert("URL", "Informe a url");
+        }
+        console.log({category, name, url});
+    }
+    
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -18,14 +39,13 @@ export default function add(){
             </View>
 
             <Text style={styles.label}>Selecione uma categoria</Text>
-            <Categories></Categories>
+            <Categories onChange={setCategory} selected={category} ></Categories>
 
             <View style={styles.form}>
-                <Input placeholder="Nome" />
-                <Input placeholder="Url " />
-                <Button title="Adicionar" />
+                <Input placeholder="Nome" onChangeText={setName} />
+                <Input placeholder="Url " onChangeText={setUrl} />
+                <Button title="Adicionar" onPress={handleAdd} />
             </View>
-            
         </View>
     )
 }
